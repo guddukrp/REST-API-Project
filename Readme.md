6a728b892c22e8affe064b6f42cd71eaa9f43b3b
guddukrp <guddukp11@gmail.com>
2024-08-16 07:54:27 +0530

    fix bugs

---
+++ Readme.md
@@ -0,0 +1,13 @@
+Added Dependencies in this project
+1. Spring Web |WEB
+2. Spring Data JPA |SQL
+3. MySQL Driver |SQL
+4. Lombok |DEVELOPER TOOLS
+
+Explanation 
+1. "entity" for database table
+2. "dto" for Class object
+3. "mapper" for convert table to object
+4. "service" for CRUD operation
+5. "controller" for REST API link handle
+6. "repository" add JPA repository interface that have all CRUD methods
---
+++ src/main/java/net/javaguides/banking/service/AccountService.java
@@ -0,0 +1,19 @@
+package net.javaguides.banking.service;
+
+import net.javaguides.banking.dto.AccountDto;
+
+import java.util.List;
+
+public interface AccountService {
+    AccountDto createAccount(AccountDto account);
+
+    AccountDto getAccountById(Long id);
+
+    AccountDto deposit(Long id,double balance);
+
+    AccountDto withdraw(Long id,double balance);
+
+    List<AccountDto> getAllAccounts();
+
+    void deleteAccountById(long id);
+}
